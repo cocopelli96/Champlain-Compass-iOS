615162097ba150f835c88eef39771d4a56951324
Travis Spinelli <travisspinelli@traviss-macbook-pro.local>
2018-01-26 16:04:08 -0500

    Initial Commit

---
+++ Champlain Compass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Champlain Compass
+//
+//  Created by Travis Spinelli on 1/26/18.
+//  Copyright © 2018 Travis Spinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
